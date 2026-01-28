45019a529c51f4219132a4cb2d76ea95a11bd06a
Richard Jones Vilorio <rjvilorio10423@liceo.edu.ph>
2026-01-28 13:26:14 +0800

    ACtivity 2 Submission

--- be-webtect2/src/index.ts
+++ be-webtect2/src/index.ts
@@ -1,14 +1,11 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import studentsRoute from './students/students.route.js'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-
-app.route('/students', studentsRoute);
 
 serve({
   fetch: app.fetch,
